0a67c111e95605cba1387e3ac7955782e5f60412
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2022-11-07 13:54:55 +0300

    doing deleting and editing student processes

--- app/static/js/fillingStdInfoPage.js
+++ app/static/js/fillingStdInfoPage.js
@@ -328,84 +328,85 @@ document.querySelector("#delstudent").addEventListener("click", () => {
 
 
 const deleteOrEditstudent = (id, opration) => {
-        let studentInfo = listOfStudents;
+    let studentInfo = listOfStudents;
+    (parts).forEach(part => {
+        document.querySelector(`#checkboxPart-${part}`).checked = false;
+    });
+
+    if (opration == "edit") {
+
+        if (studentInfo[id].partsTotal != 0) {
+            (studentInfo[id].partsTotal).forEach(part => {
+                document.querySelector(`#checkboxPart-${part}`).checked = true;
+            });
+        }
+
+        // show btn
+        document.querySelector("#addstudent").style.display = "none";
+        document.querySelector("#updstudent").style.display = "block";
+        document.querySelector("#delstudent").style.display = "none";
+        // fill input
+        document.querySelector(
+            "#studentModalLongTitle"
+        ).innerHTML = `تعديل الطالب رقم ${studentInfo[id].id}`;
+        document.querySelector("#studentID").value = `${studentInfo[id].id}`;
+        document.querySelector("#studentName").value = `${studentInfo[id].name}`;
+        // disable input
+        document.querySelector("#studentID").disabled = true;
+        document.querySelector("#studentName").disabled = false;
+        (parts).forEach(part => {
+            document.querySelector(`#checkboxPart-${part}`).disabled = false;
+        });
+    } else if (opration == "delete") {
+
+        if (studentInfo[id].partsTotal != 0) {
+            (studentInfo[id].partsTotal).forEach(part => {
+                document.querySelector(`#checkboxPart-${part}`).checked = true;
+            });
+        }
+
+        // show btn
+        document.querySelector("#addstudent").style.display = "none";
+        document.querySelector("#updstudent").style.display = "none";
+        document.querySelector("#delstudent").style.display = "block";
+        // fill input
+        document.querySelector(
+            "#studentModalLongTitle"
+        ).innerHTML = `هل أنت متأكد من حذف الطالب رقم ${studentInfo[id].id}؟`;
+        document.querySelector("#studentID").value = `${studentInfo[id].id}`;
+        document.querySelector("#studentName").value = `${studentInfo[id].name}`;
+        // disable input
+        document.querySelector("#studentID").disabled = true;
+        document.querySelector("#studentName").disabled = true;
+        (parts).forEach(part => {
+            document.querySelector(`#checkboxPart-${part}`).disabled = true;
+        });
+    } else if (opration == "add") {
+        // show btn
+        document.querySelector("#addstudent").style.display = "block";
+        document.querySelector("#updstudent").style.display = "none";
+        document.querySelector("#delstudent").style.display = "none";
+        // fill input
+        document.querySelector(
+            "#studentModalLongTitle"
+        ).innerHTML = `إضافة طالب`;
+        document.querySelector("#studentID").value = ``;
+        document.querySelector("#studentName").value = ``;
         (parts).forEach(part => {
             document.querySelector(`#checkboxPart-${part}`).checked = false;
         });
-
-        if (opration == "edit") {
-
-            if (studentInfo[id].partsTotal != 0) {
-                (studentInfo[id].partsTotal).forEach(part => {
-                    document.querySelector(`#checkboxPart-${part}`).checked = true;
-                });
-            }
-
-            // show btn
-            document.querySelector("#addstudent").style.display = "none";
-            document.querySelector("#updstudent").style.display = "block";
-            document.querySelector("#delstudent").style.display = "none";
-            // fill input
-            document.querySelector(
-                "#studentModalLongTitle"
-            ).innerHTML = `تعديل الطالب رقم ${studentInfo[id].id}`;
-            document.querySelector("#studentID").value = `${studentInfo[id].id}`;
-            document.querySelector("#studentName").value = `${studentInfo[id].name}`;
-            // disable input
-            document.querySelector("#studentID").disabled = true;
-            document.querySelector("#studentName").disabled = false;
-            (parts).forEach(part => {
-                document.querySelector(`#checkboxPart-${part}`).disabled = false;
-            });
-        } else if (opration == "delete") {
-
-            if (studentInfo[id].partsTotal != 0) {
-                (studentInfo[id].partsTotal).forEach(part => {
-                    document.querySelector(`#checkboxPart-${part}`).checked = true;
-                });
-
-                // show btn
-                document.querySelector("#addstudent").style.display = "none";
-                document.querySelector("#updstudent").style.display = "none";
-                document.querySelector("#delstudent").style.display = "block";
-                // fill input
-                document.querySelector(
-                    "#studentModalLongTitle"
-                ).innerHTML = `هل أنت متأكد من حذف الطالب رقم ${studentInfo[id].id}؟`;
-                document.querySelector("#studentID").value = `${studentInfo[id].id}`;
-                document.querySelector("#studentName").value = `${studentInfo[id].name}`;
-                // disable input
-                document.querySelector("#studentID").disabled = true;
-                document.querySelector("#studentName").disabled = true;
-                (parts).forEach(part => {
-                    document.querySelector(`#checkboxPart-${part}`).disabled = true;
-                });
-            } else if (opration == "add") {
-                // show btn
-                document.querySelector("#addstudent").style.display = "block";
-                document.querySelector("#updstudent").style.display = "none";
-                document.querySelector("#delstudent").style.display = "none";
-                // fill input
-                document.querySelector(
-                    "#studentModalLongTitle"
-                ).innerHTML = `إضافة طالب`;
-                document.querySelector("#studentID").value = ``;
-                document.querySelector("#studentName").value = ``;
-                (parts).forEach(part => {
-                    document.querySelector(`#checkboxPart-${part}`).checked = false;
-                });
-                // disable input
-                document.querySelector("#studentID").disabled = false;
-                document.querySelector("#studentName").disabled = false;
-                (parts).forEach(part => {
-                    document.querySelector(`#checkboxPart-${part}`).disabled = false;
-                });
-            } else {
-                alert(
-                    "هناك خطأ ما،  تواصل مع المطور لحل المشكلة. \n\n ErrCode: 558-info"
-                );
-            }
-        }
+        // disable input
+        document.querySelector("#studentID").disabled = false;
+        document.querySelector("#studentName").disabled = false;
+        (parts).forEach(part => {
+            document.querySelector(`#checkboxPart-${part}`).disabled = false;
+        });
+    } else {
+        alert(
+            "هناك خطأ ما،  تواصل مع المطور لحل المشكلة. \n\n ErrCode: 558-info"
+        );
+    }
+}
 
 
 
@@ -422,41 +423,41 @@ const deleteOrEditstudent = (id, opration) => {
 
 
 
-        function formatTheDate(date, typeOfFormat) {
-            let hours12 = date.getHours();
-            let hours24 = hours12;
-            let minutes = date.getMinutes();
-            let seconds = date.getSeconds();
-            const ampm = hours12 >= 12 ? "م" : "ص";
-            hours12 = hours12 % 12;
-            hours12 = hours12 || 12;
-            const year = date.getFullYear();
-            let month = date.getMonth();
-            month = Number(month) + 1 < 10 ? "0" + (Number(month) + 1) : Number(month) + 1;
-            let day = date.getDate();
-            hours12 = hours12 < 10 ? "0" + hours12 : hours12;
-            hours24 = hours24 < 10 ? "0" + hours24 : hours24;
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
-            day = day < 10 ? "0" + day : day;
-            if (isNaN(year) == true) {
-                return null;
-            }
-            if (typeOfFormat == 1) {
-                const strTime =
-                    year + "/" + month + "/" + day + ", " + hours12 + ":" + minutes + ampm;
-                return strTime;
-            } else if (typeOfFormat == 3) {
-                const strTime =
-                    year + "-" + month + "-" + day + "T" + hours24 + ":" + minutes + ":" + seconds;
-                return strTime;
-            } else if (typeOfFormat == "id") {
-                const strTime =
-                    `${month}${day}${hours24}${minutes}${seconds}`;
-                return strTime;
-            } else {
-                const strTime =
-                    year + "-" + month + "-" + day;
-                return strTime;
-            }
-        }
+function formatTheDate(date, typeOfFormat) {
+    let hours12 = date.getHours();
+    let hours24 = hours12;
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    const ampm = hours12 >= 12 ? "م" : "ص";
+    hours12 = hours12 % 12;
+    hours12 = hours12 || 12;
+    const year = date.getFullYear();
+    let month = date.getMonth();
+    month = Number(month) + 1 < 10 ? "0" + (Number(month) + 1) : Number(month) + 1;
+    let day = date.getDate();
+    hours12 = hours12 < 10 ? "0" + hours12 : hours12;
+    hours24 = hours24 < 10 ? "0" + hours24 : hours24;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    day = day < 10 ? "0" + day : day;
+    if (isNaN(year) == true) {
+        return null;
+    }
+    if (typeOfFormat == 1) {
+        const strTime =
+            year + "/" + month + "/" + day + ", " + hours12 + ":" + minutes + ampm;
+        return strTime;
+    } else if (typeOfFormat == 3) {
+        const strTime =
+            year + "-" + month + "-" + day + "T" + hours24 + ":" + minutes + ":" + seconds;
+        return strTime;
+    } else if (typeOfFormat == "id") {
+        const strTime =
+            `${month}${day}${hours24}${minutes}${seconds}`;
+        return strTime;
+    } else {
+        const strTime =
+            year + "-" + month + "-" + day;
+        return strTime;
+    }
+}
