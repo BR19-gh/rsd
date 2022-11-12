f227ce4aa9486936501cf8a58237f87fca662b11
Ibrahim Alkhowaiter <ibrahim-abdalaziz@hotmail.com>
2022-11-12 17:54:26 +0300

    adding recordPage functionalities

--- app/static/js/fillingStdRecordsPage.js
+++ app/static/js/fillingStdRecordsPage.js
@@ -217,7 +217,7 @@ const submitRecords = () => {
         x = finalReturn;
         console.log(finalReturn)
         let i = 0;
-        (finalReturn.length == Object.keys(listOfStudents).length)
+        console.log(finalReturn.length == Object.keys(listOfStudents).length)
         if (finalReturn.length == Object.keys(listOfStudents).length) {
             (Object.keys(listOfStudents)).forEach(studentId => {
                 fetch("/record", {
